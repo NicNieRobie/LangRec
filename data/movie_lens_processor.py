3f4d78913918255c1c7dfc0816089f20dab2569a
Vladislav Pendishchuk <v.pendishuk@yandex.ru>
2025-06-07 21:46:20 +0300

    Add MovieLens processor for pair-wise recommendation

---
+++ data/movie_lens_processor.py
@@ -0,0 +1,53 @@
+import os.path
+
+import pandas as pd
+
+from data.timed_interaction_processor import TimedInteractionProcessor
+
+
+class MovieLensProcessor(TimedInteractionProcessor):
+    DATASET_NAME = 'movielens'
+
+    ITEM_ID_COL = 'movieId'
+    USER_ID_COL = 'userId'
+    HISTORY_COL = 'his'
+    LABEL_COL = 'click'
+    DATE_COL = 'timestamp'
+
+    RATING_COL = 'rating'
+
+    POS_SAMPLE_COUNT = 2
+
+    NUM_TEST = 20_000
+    NUM_FINETUNE = 0
+
+    CAST_TO_STRING = False
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self._interactions = None
+
+    @property
+    def default_attrs(self):
+        return ['title']
+
+    def load_items(self) -> pd.DataFrame:
+        path = os.path.join(self.data_path, 'movies.csv')
+
+        movies = pd.read_csv(path, sep=',', encoding='ISO-8859-1')
+
+        movies['title'] = movies['title'].str.replace(r'[^A-Za-z0-9 ]+', '')
+
+        return movies
+
+    def load_users(self) -> pd.DataFrame:
+        path = os.path.join(self.data_path, 'ratings.csv')
+
+        interactions = pd.read_csv(path, sep=',')
+
+        interactions = interactions[interactions[self.RATING_COL] != 3].copy()
+        interactions[self.LABEL_COL] = interactions[self.RATING_COL] > 3
+        interactions.drop(columns=[self.RATING_COL], inplace=True)
+
+        return self._load_users(interactions)
---
+++ data/timed_interaction_processor.py
@@ -0,0 +1,50 @@
+import abc
+
+import pandas as pd
+
+from data.seq_to_label_processor import SequenceToLabelProcessor
+from utils.class_library import ignore_discovery
+
+
+@ignore_discovery
+class TimedInteractionProcessor(SequenceToLabelProcessor, abc.ABC):
+    DATE_COL: str
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self._interactions = None
+
+    def _load_users(self, interactions):
+        item_id_set = set(self.items[self.ITEM_ID_COL].unique())
+
+        interactions = interactions[interactions[self.ITEM_ID_COL].isin(item_id_set)]
+        interactions = (
+            interactions
+            .groupby(self.USER_ID_COL)
+            .filter(lambda df: df[self.LABEL_COL].nunique() == 2)
+        )
+
+        self._interactions = interactions
+
+        pos_interactions = interactions[interactions[self.LABEL_COL] == 1]
+
+        users = (
+            pos_interactions.sort_values([self.USER_ID_COL, self.DATE_COL])
+            .groupby(self.USER_ID_COL)[self.ITEM_ID_COL]
+            .apply(list)
+            .reset_index(name=self.HISTORY_COL)
+        )
+
+        return self._extract_pos_interactions(users)
+
+    def load_interactions(self) -> pd.DataFrame:
+        uid_set = set(self.users[self.USER_ID_COL].nunique())
+
+        neg_interactions = (
+            self._interactions[self._interactions[self.LABEL_COL] == 0]
+            .loc[lambda df: df[self.USER_ID_COL].isin(uid_set)]
+            .drop(columns=[self.DATE_COL])
+        )
+
+        return pd.concat([neg_interactions, self._pos_interactions], ignore_index=True)
