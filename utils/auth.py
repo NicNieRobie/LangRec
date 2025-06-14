e5fe170d13df0f0c1302f22e012b01b684a5cb90
Vladislav Pendishchuk <v.pendishuk@yandex.ru>
2025-06-14 04:01:41 +0300

    Support auth credentials from env vars

--- utils/auth.py
+++ utils/auth.py
@@ -1,9 +1,17 @@
-with open('.auth') as f:
-    auth_data = f.read()
+import os
 
 auth = {}
-for line in auth_data.strip().split('\n'):
-    name, key = line.split('=')
-    auth[name.strip()] = key.strip()
 
-HF_KEY = auth['hf']
+try:
+    with open('.auth') as f:
+        auth_data = f.read()
+    for line in auth_data.strip().split('\n'):
+        name, key = line.split('=')
+        auth[name.strip()] = key.strip()
+except FileNotFoundError:
+    pass
+
+HF_KEY = auth.get('hf') or os.environ.get('HF_KEY')
+
+if not HF_KEY:
+    raise RuntimeError("Hugging Face key not found.")
