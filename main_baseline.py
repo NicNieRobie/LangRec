fb1ad45e228600c665e79f1a1a2323c1ec474c0e
Vladislav Pendishchuk <v.pendishuk@yandex.ru>
2025-06-19 14:53:27 +0300

    RecBole numpy fix

--- main_baseline.py
+++ main_baseline.py
@@ -5,7 +5,18 @@ from utils.seed import seed
 from runner_baseline import BaselineRunner
 from loguru import logger
 
+import numpy as np
+
 if __name__ == '__main__':
+    if not hasattr(np, 'float_'):
+        np.float_ = np.float64
+    if not hasattr(np, 'unicode_'):
+        np.unicode_ = np.str_
+    if not hasattr(np, 'unicode'):
+        np.unicode = np.str_
+    if not hasattr(np, 'complex_'):
+        np.complex_ = np.complex128
+
     cli_config_path = os.environ.get('CLI_CONFIG_PATH', 'config/cli/baseline_cli_config.yaml')
     arg_parser = ArgParser(cli_config_path)
     config = arg_parser.parse_args()
